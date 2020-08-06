64a2506e19d2bb1566faa3ff4d5331172714d248
Anton Shevchuk <w.s.f.antonshevchuk@gmail.com>
2020-08-06 20:16:29 +0300

    project with microservices

---
+++ conference-app/server/services/Speakers.js
@@ -0,0 +1,120 @@
+const axios = require('axios');
+const url = require('url');
+const crypto = require('crypto');
+const fs = require('fs');
+const util = require('util');
+
+const fsexists = util.promisify(fs.exists);
+
+const CircuitBreaker = require('../lib/CircuitBreaker');
+
+const circuitBreaker = new CircuitBreaker();
+
+class SpeakersService {
+    constructor({ serviceRegistryUrl, serviceVersionIdentifier }) {
+        this.serviceRegistryUrl = serviceRegistryUrl;
+        this.serviceVersionIdentifier = serviceVersionIdentifier;
+        this.cache = {};
+    }
+
+    async getImage(path) {
+        const { ip, port } = await this.getService('speakers-service');
+        return this.callService({
+            method: 'get',
+            responseType: 'stream',
+            url: `http://${ip}:${port}/images/${path}`
+        });
+    }
+
+    async getNames() {
+        const { ip, port } = await this.getService('speakers-service');
+        return this.callService({
+            method: 'get',
+            url: `http://${ip}:${port}/names`
+        });
+    }
+
+    async getListShort() {
+        const { ip, port } = await this.getService('speakers-service');
+        return this.callService({
+            method: 'get',
+            url: `http://${ip}:${port}/list-short`
+        });
+    }
+
+    async getList() {
+        const { ip, port } = await this.getService('speakers-service');
+        return this.callService({
+            method: 'get',
+            url: `http://${ip}:${port}/list`
+        });
+    }
+
+    async getAllArtwork() {
+        const { ip, port } = await this.getService('speakers-service');
+        return this.callService({
+            method: 'get',
+            url: `http://${ip}:${port}/artwork`
+        });
+    }
+
+    async getSpeaker(shortname) {
+        const { ip, port } = await this.getService('speakers-service');
+        return this.callService({
+            method: 'get',
+            url: `http://${ip}:${port}/speaker/${shortname}`
+        });
+    }
+
+    async getArtworkForSpeaker(shortname) {
+        const { ip, port } = await this.getService('speakers-service');
+        return this.callService({
+            method: 'get',
+            url: `http://${ip}:${port}/artwork/${shortname}`
+        });
+    }
+
+    async callService(requestOptions) {
+        const servicePath = url.parse(requestOptions.url).path;
+        const cacheKey = crypto
+            .createHash('md5')
+            .update(requestOptions.method + servicePath)
+            .digest('hex');
+        let cacheFIle = null;
+
+        if (
+            requestOptions.responseType &&
+            requestOptions.responseType === 'stream'
+        ) {
+            cacheFIle = `${__dirname}/../../_imagecache/${cacheKey}`;
+        }
+
+        const result = await circuitBreaker.callService(requestOptions);
+
+        if (!result) {
+            if (this.cache[cacheKey]) return this.cache[cacheKey];
+            if (cacheFIle) {
+                const exists = await fsexists(cacheFIle);
+                if (exists) return fs.createReadStream(cacheFIle);
+            }
+            return false;
+        }
+
+        if (!cacheFIle) {
+            this.cache[cacheKey] = result;
+        } else {
+            const ws = fs.createWriteStream(cacheFIle);
+            result.pipe(ws);
+        }
+        return result;
+    }
+
+    async getService(servicename) {
+        const response = await axios.get(
+            `${this.serviceRegistryUrl}/find/${servicename}/${this.serviceVersionIdentifier}`
+        );
+        return response.data;
+    }
+}
+
+module.exports = SpeakersService;
