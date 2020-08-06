7c8e5160febcb0791e71a4836a4e0b7162fb291a
anthonySensei <w.s.f.antonshevchuk@gmail.com>
2020-08-06 21:24:29 +0300

    added possibility to add feedbacks with queue

--- feedback-service/server/service.js
+++ feedback-service/server/service.js
@@ -1,39 +1,60 @@
 const express = require('express');
+const amqplib = require('amqplib');
 
 const Feedback = require('./lib/Feedback');
 
 const service = express();
 
 module.exports = (config) => {
-  const feedback = new Feedback(config.data.feedback);
+    const feedback = new Feedback(config.data.feedback);
 
-  const log = config.log();
-  // Add a request logging middleware in development mode
-  if (service.get('env') === 'development') {
-    service.use((req, res, next) => {
-      log.debug(`${req.method}: ${req.url}`);
-      return next();
-    });
-  }
+    const log = config.log();
 
-  service.get('/list', async (req, res, next) => {
-    try {
-      return res.json(await feedback.getList());
-    } catch (err) {
-      return next(err);
+    const q = 'feedback';
+    amqplib
+        .connect('amqp://localhost')
+        .then((conn) => conn.createChannel())
+        .then((ch) =>
+            ch.assertQueue(q).then(() =>
+                ch.consume(q, (msg) => {
+                    if (msg) {
+                        log.debug(`Got message ${msg.content.toString()}`);
+                        const qm = JSON.parse(msg.content.toString());
+                        feedback
+                            .addEntry(qm.name, qm.title, qm.message)
+                            .then(() => ch.ack(msg));
+                    }
+                })
+            )
+        )
+        .catch((err) => log.fatal(err));
+
+    // Add a request logging middleware in development mode
+    if (service.get('env') === 'development') {
+        service.use((req, res, next) => {
+            log.debug(`${req.method}: ${req.url}`);
+            return next();
+        });
     }
-  });
 
-  // eslint-disable-next-line no-unused-vars
-  service.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    // Log out the error to the console
-    log.error(error);
-    return res.json({
-      error: {
-        message: error.message,
-      },
+    service.get('/list', async (req, res, next) => {
+        try {
+            return res.json(await feedback.getList());
+        } catch (err) {
+            return next(err);
+        }
     });
-  });
-  return service;
+
+    // eslint-disable-next-line no-unused-vars
+    service.use((error, req, res, next) => {
+        res.status(error.status || 500);
+        // Log out the error to the console
+        log.error(error);
+        return res.json({
+            error: {
+                message: error.message
+            }
+        });
+    });
+    return service;
 };
